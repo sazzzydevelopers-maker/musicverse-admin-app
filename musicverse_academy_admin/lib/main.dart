78b9a0ea825ddee79029d043384a617b07dd9983
sairamshanker970-dotcom <sazzzydevelopers@gmail.com>
2026-08-08 21:39:38 +0530

    auth done

--- musicverse_academy_admin/lib/main.dart
+++ musicverse_academy_admin/lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
+
 import 'package:musicverse_academy_admin/core/constants/app_colors.dart';
 import 'package:musicverse_academy_admin/features/auth/providers/auth_provider.dart';
 import 'package:musicverse_academy_admin/routes/app_router.dart';
+import 'package:musicverse_academy_admin/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MusicVerseAdminApp());
 }
 
